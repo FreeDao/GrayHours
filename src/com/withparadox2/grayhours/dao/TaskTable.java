16fc671340f355e7a6be34bbe1ca58c93eb04879
withparadox2 <withparadox2@gmail.com>
2014-02-20 18:13:07 +0800

    add structure

---
+++ src/com/withparadox2/grayhours/dao/TaskTable.java
@@ -0,0 +1,12 @@
+package com.withparadox2.grayhours.dao;
+
+/**
+ * Created by Administrator on 14-2-20.
+ */
+public class TaskTable {
+	public static final String TABLE_NAME = "task_table";
+	public static final String KEY_ID = "_id";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_START_TIME = "start_time";
+	public static final String KEY_TOTAL_TIME = "total_time";
+}
