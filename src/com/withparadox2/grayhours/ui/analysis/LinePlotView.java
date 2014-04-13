a341e5b3c84d5c0f11ef0f1b3285cf08960c1a70
withparadox2 <withparadox2@gmail.com>
2014-04-13 22:05:26 +0800

    add fling gesture

--- src/com/withparadox2/grayhours/ui/analysis/LinePlotView.java
+++ src/com/withparadox2/grayhours/ui/analysis/LinePlotView.java
@@ -3,14 +3,14 @@ package com.withparadox2.grayhours.ui.analysis;
 import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.*;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.OverScroller;
+import android.widget.Scroller;
 import com.withparadox2.grayhours.R;
 import com.withparadox2.grayhours.support.AnalysisTool;
 import com.withparadox2.grayhours.support.CalendarTool;
@@ -23,12 +23,13 @@ import java.util.Random;
 /**
  * Created by withparadox2 on 14-3-3.
  */
-public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateListener{
+public class LinePlotView extends View {
 	private Paint gridPaint;
 	private Paint labelPaint;
 	private Paint dataLinePaint;
 	private Paint framePaint;
 
+	private Scroller mScroller;
 	private int mLabelSeparation;
 	private int mLabelWidth;
 	private int mLabelHeight;
@@ -49,6 +50,8 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 
 	private Rect contentRect = new Rect();
 
+	private float flingStartX;
+
 	public LinePlotView(Context context) {
 		this(context, null, 0);
 	}
@@ -60,6 +63,7 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 	public LinePlotView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		initialPaint();
+		mScroller = new Scroller(context);
 		map = AnalysisTool.getDataMapTest(0);
 		gestureDetector = new GestureDetector(context, new MyOnGestureListener());
 		this.setLongClickable(true);
@@ -75,8 +79,8 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 		widthOfView = getWidth();
 		heightOfView = getHeight();
 
-		cellHeight = contentRect.height()/12;
-		cellWidth = contentRect.width()/7;
+		cellHeight = contentRect.height()/12f;
+		cellWidth = contentRect.width()/7f;
 	}
 
 	private void initialPaint(){
@@ -113,7 +117,7 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 		canvas.drawRect(contentRect, framePaint);
 
 		canvas.clipRect(contentRect);
-//		drawDataLine(canvas, labelPaint);
+		drawDataLine(canvas, labelPaint);
 		drawBackgroundGrid(canvas, gridPaint);
 		canvas.restore();
 
@@ -131,11 +135,12 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 
 	private void drawBackgroundGrid(Canvas canvas, Paint paint){
 		float y = contentRect.top;
-		for (int i=0; i<=11; i++){
+		for (int i=0; i<=12; i++){
 			canvas.drawLine(contentRect.left, y, contentRect.right, y, paint);
 			y += cellHeight;
 		}
-		float x = contentRect.left - scrollOffSet + cellWidth * (int) (scrollOffSet/cellWidth);
+		int scrollCellWidthNum = (int)(scrollOffSet / cellWidth);
+		float x = (float)contentRect.left - scrollOffSet + cellWidth*scrollCellWidthNum;
 		for (int i=0; i<=7; i++){
 			canvas.drawLine(x, contentRect.top, x, contentRect.bottom, paint);
 			x += cellWidth;
@@ -156,7 +161,7 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 	private void drawHorizontalLabel(Canvas canvas, Paint paint){
 		int scrollCellWidthNum = (int)(scrollOffSet / cellWidth);
 		float width = contentRect.left - scrollOffSet + cellWidth*scrollCellWidthNum;
-		for (int i=0; i < 7; i++){
+		for (int i=0; i <= 7; i++){
 			canvas.drawText(CalendarTool.getDateFromToday(i + scrollCellWidthNum).substring(8),
 					width ,
 					getHeight(),
@@ -165,43 +170,36 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 		}
 	}
 
-/*	private void drawDataLine(Canvas canvas, Paint paint){
+	private void drawDataLine(Canvas canvas, Paint paint){
 		int scrollCellWidthNum = (int)(scrollOffSet / cellWidth);
 		int newKey, oldKey;
-		float newX, oldX;
+		float x = contentRect.left - scrollOffSet + cellWidth*scrollCellWidthNum;
 		for (int i=0; i <= 8; i++){
 			newKey = CalendarTool.getDateIntervalFromBase(CalendarTool.getDateFromToday(i + scrollCellWidthNum));
 			oldKey = CalendarTool.getDateIntervalFromBase(CalendarTool.getDateFromToday(i-1 + scrollCellWidthNum));
-			newX = gridLinePaddintLeft + i*cellWidth - scrollOffSet + cellWidth*scrollCellWidthNum;
-			oldX = gridLinePaddintLeft + (i-1)*cellWidth - scrollOffSet + cellWidth*scrollCellWidthNum;
 			if (!map.containsKey(newKey)){
 				map.put(newKey,new Random().nextInt(24*60));
 			}
 			if (!map.containsKey(oldKey)){
 				map.put(oldKey,new Random().nextInt(24*60));
 			}
-
-
-			canvas.drawLine(newX,
-					gridLinePaddintTop + map.get(newKey)/120f * cellHeight,
-					oldX,
-					gridLinePaddintTop + map.get(oldKey)/120f * cellHeight,
+			canvas.drawLine(x,
+					contentRect.top + map.get(newKey)/120f * cellHeight,
+					x-cellWidth,
+					contentRect.top + map.get(oldKey)/120f * cellHeight,
 					paint);
+			x += cellWidth;
 
 		}
 
 
-	}*/
+	}
 
 
 	public void setIndex(int index){
 		this.index = index;
 	}
 
-	@Override
-	public void onAnimationUpdate(ValueAnimator animation) {
-
-	}
 
 	class MyOnGestureListener extends GestureDetector.SimpleOnGestureListener{
 		@Override
@@ -213,16 +211,42 @@ public class LinePlotView extends View implements ValueAnimator.AnimatorUpdateLi
 		@Override
 		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
 			scrollOffSet = scrollOffSet + distanceX;
-//			DebugConfig.log("onScroll has been called " + scrollOffSet);
-			invalidate();
-			return super.onScroll(e1, e2, distanceX, distanceY);
+			ViewCompat.postInvalidateOnAnimation(LinePlotView.this);
+			return true;
 		}
 
-//		@Override
-//		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//			DebugConfig.log("onFling has been called");
-//			scrollOffSet = scrollOffSet + velocityX;
-//			return super.onFling(e1, e2, velocityX, velocityY);
-//		}
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			flingStartX = e2.getX();
+			fling((int) -velocityX, (int) -velocityY);
+			return true;
+		}
+	}
+
+	private void fling(int velocityX, int velocityY) {
+
+		// Before flinging, aborts the current animation.
+		mScroller.forceFinished(true);
+		// Begins the animation
+		mScroller.fling(
+				// Current scroll position
+				(int) scrollOffSet,
+				0,
+				velocityX,
+				velocityY,
+				-100000, 800000,
+				0, 0);
+		// Invalidates to trigger computeScroll()
+		ViewCompat.postInvalidateOnAnimation(this);
+	}
+
+	@Override
+	public void computeScroll() {
+		super.computeScroll();
+		if (mScroller.computeScrollOffset()) {
+			scrollOffSet = mScroller.getCurrX();
+			ViewCompat.postInvalidateOnAnimation(this);
+		}
+//		DebugConfig.log("onFling has been called " +  mScroller.getCurrX());
 	}
 }
